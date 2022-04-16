5035dbd7c5ac834717ed1e46e8b532f1391ab20e
Tarun Vishal <tv51555@gmail.com>
2022-04-16 15:40:13 -0400

    Removed Unnecessary Files

--- src/components/explore/ExploreScreen.js
+++ src/components/explore/ExploreScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavigationSidebar from "../NavigationSidebar";
 import ExploreComponent from "./ExploreComponent";
-import WhoToFollowList from "../WhoToFollowList";
+import WhoToFollowList from "../../WhoToFollowList";
 const ExploreScreen = () => {
   return(
       <div className="row mt-2">
