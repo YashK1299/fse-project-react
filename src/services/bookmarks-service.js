22aaaba467fd082bcaf73340da81a56c65c9d661
Yash Kothari <kothariy3@gmail.com>
2022-04-27 13:56:12 -0400

    Renamed REACT_APP_BASE_URL to NODE_APP_BASE_URL

--- src/services/bookmarks-service.js
+++ src/services/bookmarks-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.NODE_APP_BASE_URL;
 
 const api = axios.create({
     withCredentials: true
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.NODE_APP_BASE_URL;
 
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
